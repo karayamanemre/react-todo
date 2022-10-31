564bae52cf32f1ef4e4b05a8fe38c1e86fc952be
Emre Karayaman <karayaman.emre@outlook.com>
2022-10-31 13:07:34 +0300

    router install

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './functionBased/App.css';
+import { BrowserRouter } from 'react-router-dom';
 //component file
 import TodoContainer from './functionBased/components/ToDoContainer';
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
